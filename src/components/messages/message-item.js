d2f1b2706a4f589547be5e7bc38e769ccb5a3412
sghosalkar <shubhamghosalkar.sg@gmail.com>
2022-04-07 00:09:46 -0400

    Added validations and avatars

--- src/components/messages/message-item.js
+++ src/components/messages/message-item.js
@@ -6,10 +6,11 @@ import axios from "axios";
  * Represents the message component of the chat section
  * @param messageItem message object
  * @param me logged in user
+ * @param recipient recipient user object
  * @returns {JSX.Element} react component
  * @constructor
  */
-const MessageItem = ({messageItem, me}) => {
+const MessageItem = ({messageItem, me, recipient}) => {
   /**
    * Downloads the attachment in the message item
    */
@@ -35,16 +36,16 @@ const MessageItem = ({messageItem, me}) => {
       {
         messageItem.recipient === me._id && (
           messageItem.attachmentKey ?
-            <div className="w-100 d-flex">
-              <img className="avatar me-2" src="/images/alice.jpg" alt=""/>
+            <div className="w-100 d-flex align-items-center">
+              <img className="avatar me-2 bg-secondary bg-opacity-50" src={`https://avatars.dicebear.com/api/adventurer/${recipient.username}.svg`} alt=""/>
               <div className="p-2 text-break overflow-auto bg-secondary bg-opacity-25 d-flex align-items-center">
                 <div className="px-2 round-icon bg-secondary bg-opacity-50" onClick={download}><i className="fa fa-download"/></div>
                 <div className="ps-2">{messageItem.message}</div>
               </div>
             </div>
            :
-            <div className="w-100 d-flex">
-              <img className="avatar me-2" src="/images/alice.jpg" alt=""/>
+            <div className="w-100 d-flex align-items-center">
+              <img className="avatar me-2 bg-secondary bg-opacity-50" src={`https://avatars.dicebear.com/api/adventurer/${recipient.username}.svg`} alt=""/>
               <div className="p-2 text-break overflow-auto bg-secondary bg-opacity-25">
                 {messageItem.message}
               </div>
@@ -54,19 +55,19 @@ const MessageItem = ({messageItem, me}) => {
       {
         messageItem.sender === me._id && (
           messageItem.attachmentKey ?
-            <div className="w-100 d-flex justify-content-end">
+            <div className="w-100 d-flex justify-content-end align-items-center">
               <div className="p-2 text-break overflow-auto bg-secondary bg-opacity-25 d-flex align-items-center">
                 <div className="px-2 round-icon bg-secondary bg-opacity-50" onClick={download}><i className="fa fa-download"/></div>
                 <div className="ps-2">{messageItem.message}</div>
               </div>
-              <img className="avatar ms-2" src="/images/bob.jpg" alt=""/>
+              <img className="avatar ms-2 bg-secondary bg-opacity-50" src={`https://avatars.dicebear.com/api/adventurer/${me.username}.svg`} alt=""/>
             </div>
             :
-            <div className="w-100 d-flex justify-content-end">
+            <div className="w-100 d-flex justify-content-end align-items-center">
               <div className="p-2 text-break overflow-auto bg-secondary bg-opacity-25">
                 {messageItem.message}
               </div>
-              <img className="avatar ms-2" src="/images/bob.jpg"/>
+              <img className="avatar ms-2 bg-secondary bg-opacity-50" src={`https://avatars.dicebear.com/api/adventurer/${me.username}.svg`} alt=""/>
             </div>
         )
       }
