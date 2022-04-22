701917289568be6d6ae2ee9e1485dfcbac18f37f
tomiris <kozhabekkyzy.t@northeastern.edu>
2022-04-22 17:25:22 -0400

    Tested send message feature

--- src/tests/message-service.test.js
+++ src/tests/message-service.test.js
@@ -4,7 +4,7 @@ import {deleteMessage, sendMessage} from "../services/messages-service";
 describe('can send with REST API', () => {
   // sample user: the sender of the message
   const test_user1 = {
-    _id: '6250af9334a1c846721967f9',
+    _id: '6254809012c1e7ba29312909',
     username: 'test_user1',
     password: 'test123',
     email: 'user1@test.com'
@@ -12,21 +12,20 @@ describe('can send with REST API', () => {
 
   // sample user: the recipient of the message
   const test_user2 = {
-    _id: '6251c3e6e83dfdcb6dd3a661',
+    _id: '6254ac15d28b40132d8aded8',
     username: 'test_user2',
     password: 'test123',
     email: 'user2@test.com'
   };
 
-  // sample story to insert
+  // sample message body to insert
   const message = "Hi! How are you?";
-
+  //store the ID of the message created to delete after the test
   let messageID = "";
 
   // setup test before running test
   beforeAll(async () => {
-    // remove users and stories to make sure we create it in the test
-    //await deleteMessage(messageID);
+    // remove users and messages to make sure we create it in the test
     await deleteUser(test_user1._id);
     return await deleteUser(test_user2._id);
   });
