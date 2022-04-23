974fdf663cd59a30487656dc38601a85866f4aa0
Galen Otten <galen.louise@gmail.com>
2022-04-23 00:02:30 -0400

    adding more group unit tests

--- src/tests/groups-service.test.js
+++ src/tests/groups-service.test.js
@@ -1,11 +1,11 @@
-import {api, createGroup} from "../services/groups-service";
+import {api, createGroup, deleteGroup} from "../services/groups-service";
 
 const MOCK_GROUPS = [
     {
         _id: "11",
         name: "Group 1",
         owner: 4,
-        users: [3, 4, 5]
+        users: [2, 3, 4, 5]
     },
     {
         _id: "12",
@@ -14,6 +14,7 @@ const MOCK_GROUPS = [
         users: [1, 2]
     }
 ];
+
 
 describe('Create Group', () => {
     test("Create Group using API", async () => {
@@ -33,6 +34,25 @@ describe('Create Group', () => {
         expect(resp).toEqual(MOCK_GROUPS[0]);
         const params = {users: group.users, group: group};
         expect(groupApiMock).toBeCalledWith(`${process.env.REACT_APP_BASE_URL}/api/users/4/groups`, params);
+        groupApiMock.mockRestore();
+    });
+});
+
+
+describe("Delete group", () => {
+    test("Delete a group using API", async () => {
+        const apiResponse = JSON.parse(
+            JSON.stringify(MOCK_GROUPS[1])
+        );
+        const groupApiMock = jest.spyOn(api, "delete");
+        groupApiMock.mockImplementation(() => {
+            return Promise.resolve({ data: apiResponse });
+        });
+
+        const groupId = "12";
+        const resp = await deleteGroup(groupId);
+        expect(resp).toEqual(apiResponse);
+        expect(groupApiMock).toBeCalledWith(`${process.env.REACT_APP_BASE_URL}/api/groups/${groupId}`);
         groupApiMock.mockRestore();
     });
 });
