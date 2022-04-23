a8e4287dbbc64a9be70e87c96b24e0511ffb0052
Sanat Deshpande <sanatdeshpande1@gmail.com>
2022-04-22 22:12:47 -0400

    Added unit tests for group messages

--- src/tests/group-message-service.test.js
+++ src/tests/group-message-service.test.js
@@ -5,17 +5,6 @@ import {
   deleteGroupMessage,
 } from "../services/messages-service";
 
-const MEDIA_MAPPING = [
-  {
-    image: "Some Image Blob",
-    key: "111",
-  },
-  {
-    image: "Some Different Image Blob",
-    key: "112",
-  },
-];
-
 const MOCK_MESSAGES = [
   {
     message: "The Chamber of Secrets!",
@@ -44,50 +33,50 @@ const MOCK_MESSAGES_FROM_SERVER = [
   },
 ];
 
-// describe("Find group messages", () => {
-//   test("Find group messages of a particular group using API", async () => {
-//     const apiResponse = JSON.parse(JSON.stringify(MOCK_MESSAGES_FROM_SERVER));
-//     const messageApiMock = jest.spyOn(api, "get");
-//     messageApiMock.mockImplementation(() => {
-//       return Promise.resolve({ data: apiResponse });
-//     });
+describe("Find group messages", () => {
+  test("Find group messages of a particular group using API", async () => {
+    const apiResponse = JSON.parse(JSON.stringify(MOCK_MESSAGES_FROM_SERVER));
+    const messageApiMock = jest.spyOn(api, "get");
+    messageApiMock.mockImplementation(() => {
+      return Promise.resolve({ data: apiResponse });
+    });
 
-//     const groupId = "2";
-//     const resp = await getGroupMessages(groupId);
-//     expect(resp).toEqual(apiResponse);
-//     expect(messageApiMock).toBeCalledWith(
-//       `${process.env.REACT_APP_BASE_URL}/api/groups/${groupId}/messages`
-//     );
-//     messageApiMock.mockRestore();
-//   });
-// });
+    const groupId = "2";
+    const resp = await getGroupMessages(groupId);
+    expect(resp).toEqual(apiResponse);
+    expect(messageApiMock).toBeCalledWith(
+      `${process.env.REACT_APP_BASE_URL}/api/groups/${groupId}/messages`
+    );
+    messageApiMock.mockRestore();
+  });
+});
 
-// describe("Send group message", () => {
-//   test("Send group message using API", async () => {
-//     const apiResponse = JSON.parse(
-//       JSON.stringify(MOCK_MESSAGES_FROM_SERVER[0])
-//     );
-//     const messageApiMock = jest.spyOn(api, "post");
-//     messageApiMock.mockImplementation(() => {
-//       return Promise.resolve({ data: apiResponse });
-//     });
+describe("Send group message", () => {
+  test("Send group message using API", async () => {
+    const apiResponse = JSON.parse(
+      JSON.stringify(MOCK_MESSAGES_FROM_SERVER[0])
+    );
+    const messageApiMock = jest.spyOn(api, "post");
+    messageApiMock.mockImplementation(() => {
+      return Promise.resolve({ data: apiResponse });
+    });
 
-//     const message = MOCK_MESSAGES[0];
-//     const messageFromServer = apiResponse;
-//     const resp = await sendGroupMessage(
-//       message.sender,
-//       message.group,
-//       message.message,
-//       null
-//     );
-//     expect(resp).toEqual(messageFromServer);
-//     expect(messageApiMock).toBeCalledWith(
-//       `${process.env.REACT_APP_BASE_URL}/api/groups/${messageFromServer.group}/users/${messageFromServer.sender}/messages`,
-//       { message: message.message }
-//     );
-//     messageApiMock.mockRestore();
-//   });
-// });
+    const message = MOCK_MESSAGES[0];
+    const messageFromServer = apiResponse;
+    const resp = await sendGroupMessage(
+      message.sender,
+      message.group,
+      message.message,
+      null
+    );
+    expect(resp).toEqual(messageFromServer);
+    expect(messageApiMock).toBeCalledWith(
+      `${process.env.REACT_APP_BASE_URL}/api/groups/${messageFromServer.group}/users/${messageFromServer.sender}/messages`,
+      { message: message.message }
+    );
+    messageApiMock.mockRestore();
+  });
+});
 
 describe("Delete group messages", () => {
   test("Delete a group message using API", async () => {
